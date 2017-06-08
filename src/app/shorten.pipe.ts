e25fe092c92f9fc673bf2e28f9a0504f22dab791
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-06-08 14:41:32 +0200

    Parameterzing the custom pipe

--- src/app/shorten.pipe.ts
+++ src/app/shorten.pipe.ts
@@ -4,9 +4,9 @@ import {Pipe, PipeTransform} from '@angular/core';
   name: 'ShortenPipe'
 })
 export class ShortenPipe implements PipeTransform {
-  transform(value: any) {
-    if (value.length > 15) {
-      return value.substr(0, 15) + '...';
+  transform(value: any, limit: number) {
+    if (value.length > limit) {
+      return value.substr(0, limit) + '...';
     } else {
       return value;
     }
