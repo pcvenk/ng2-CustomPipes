31aa90f40498f11e72f5f647a311ca1de778de3a
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-06-08 15:35:38 +0200

    Added filter pipe to search for individual item or list of items

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -6,17 +6,16 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FilterPipe implements PipeTransform {
 
   transform(value: any, enteredString: string): any {
-    if (value.length === 0) {
+    if (value.length === 0 || enteredString === '') {
       return value;
     }
     let filteredAray = [];
     for (let item of value) {
       // checking the server status
-      if (item.status === enteredString) {
+      if (item.status === enteredString || item.name === enteredString) {
         filteredAray.push(item);
       }
     }
     return filteredAray;
   }
-
 }
